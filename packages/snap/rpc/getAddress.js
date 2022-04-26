adbe9c5be0513129cb77f624f1714171456dda90
aqrare <takumitakumi0310@gmail.com>
2022-04-26 02:51:41 +0200

    add snap template

---
+++ packages/snap/rpc/getAddress.js
@@ -0,0 +1,12 @@
+// import {Wallet} from "../interfaces";
+// import {getKeyPair} from "../filecoin/account";
+
+// export async function getAddress(wallet: Wallet): Promise<string> {
+//   const keyPair = await getKeyPair(wallet);
+//   return keyPair.address;
+// }
+
+export async function getAddress() {
+  const address = "0x65150B5Fa861481651225Ef4412136DCBf696232"
+  return address;
+}
---
+++ packages/snap/rpc/getBalance.js
@@ -0,0 +1,17 @@
+// import {Wallet} from "../interfaces";
+// import {getKeyPair} from "../filecoin/account";
+// import {LotusRpcApi} from "../filecoin/types";
+// import {FilecoinNumber} from '@glif/filecoin-number/dist';
+
+// export async function getBalance(wallet: Wallet, api: LotusRpcApi, address?: string): Promise<string> {
+//   if(!address) {
+//     address = (await getKeyPair(wallet)).address;
+//   }
+//   const balance = await api.walletBalance(address);
+//   return (new FilecoinNumber(balance, 'attofil')).toFil();
+// }
+
+export async function getBalance() {
+  const balance = "345"
+  return balance
+}
