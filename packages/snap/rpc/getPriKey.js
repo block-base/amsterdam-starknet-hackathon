adccb359f5eecda309f2f427e845612ee3af8de0
Taiju Sanagi <taijusanagi@gmail.com>
2022-04-26 04:13:35 +0200

    chore: make it bit clean

--- packages/snap/rpc/getPriKey.js
+++ packages/snap/rpc/getPriKey.js
@@ -1,28 +1,22 @@
-// const bip44Node = await wallet.request({
-//     method: `snap_getBip44Entropy_${bip44Code}`,
-//     params: []
-//   }) as JsonBIP44CoinTypeNode;
 export const getPriKey = async () => {
-   const responce = await wallet.request({
-    method: 'snap_confirm',
+  const responce = await wallet.request({
+    method: "snap_confirm",
     params: [
-        {
+      {
         prompt: `Export Private Key`,
-        description:
-            'Do you want to export Private Key?',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
-      });
+        description: "Do you want to export Private Key?",
+        textAreaContent: "But you can edit the snap source code to make it do something, if you want to!",
+      },
+    ],
+  });
 
-    if (responce) {
-        const derivationPath = "m/44'/461'/0'/0/0"
-        const [, , coinType, account, change, addressIndex] = derivationPath.split('/');
-        const bip44Code = coinType.replace("\'", "");
-        return await wallet.request({
-              method: `snap_getBip44Entropy_${bip44Code}`,
-              params: []
-          })
+  if (responce) {
+    const derivationPath = "m/44'/461'/0'/0/0";
+    const [, , coinType, account, change, addressIndex] = derivationPath.split("/");
+    const bip44Code = coinType.replace("'", "");
+    return await wallet.request({
+      method: `snap_getBip44Entropy_${bip44Code}`,
+      params: [],
+    });
   }
 };
--- packages/snap/rpc/getStarkKey.js
+++ packages/snap/rpc/getStarkKey.js
@@ -1,13 +1,4 @@
-// import {Wallet} from "../interfaces";
-// import {getKeyPair} from "../filecoin/account";
-
-// export async function getPublicKey(wallet: Wallet): Promise<string> {
-//   const keyPair = await getKeyPair(wallet);
-//   return keyPair.publicKey;
-// }
-
-
-export async function getStarkKey(){
-  const publicKey = "pubkey"
+export async function getStarkKey() {
+  const publicKey = "pubkey";
   return publicKey;
 }
